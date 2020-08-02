263aa1c70c30344703dcb8fdea3d684ddbcbae5d
test <test@qq.com>
2020-08-02 11:55:53 +0800

    bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="hello">
     <p>{{ msg }}</p>
+	<!--
 	<p>{{ project }}</p>
+	<input v-model="input_message" placeholder="编辑我……" />
+	<p>消息是: {{ input_message }}</p>
+	
+	<button @click="addOne">增加 1 次</button>
+	<p>这个按钮被点击了 {{ counter }} 次。</p>
+	<ol>
+	  <li v-for="site in sites">
+		{{ site.name }}
+	  </li>
+	</ol>
+	<p v-if="seen">这段显示与否，取决于seen。</p>
+	<template v-if="ok">
+	  <p>template里控制显示，取决于ok。</p>
+	  <p>人生若只如初见,何事秋风悲画扇。</p>
+	  <p>等闲变却故人心,却道故人心易变。</p>
+	</template>-->
+	<p>原始字符串: {{ project }}</p>
+	<p>计算后反转字符串: {{ reversedProject }}</p>
   </div>
 </template>
 
@@ -13,14 +32,36 @@ export default {
   },
   data() {
     return {
-      project: 'vue-demo'
+      project: 'vue-demo',
+	  input_message: '应用名称',
+	  counter: 0,
+	  sites: [
+		{ name: 'baidu' },
+		{ name: 'alibaba' },
+		{ name: 'ticktok' },
+		{ name: 'tecent' }
+	  ],
+	  seen: false,
+	  ok: true
     }
+  },
+  methods: {
+	addOne: function (event) {
+	  this.counter += 1;
+    }
+  },
+  computed: {
+	// 计算属性的 getter
+	reversedProject: function () {
+	// `this` 指向 vm 实例
+	  return this.project.split('').reverse().join('')
+	},
   }
 }
 </script>
 
 <style scoped>
-p {
+.hello {
   text-align: left;
 }
 </style>
